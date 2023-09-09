df3c1f370fcc162b60156cfcc7d9660b2705572e
BrunoKlein504 <bkleingomes@gmail.com>
2023-09-09 18:52:40 -0300

    OrderStatus Enum

--- src/main/java/com/kleinbruno/course/entities/Order.java
+++ src/main/java/com/kleinbruno/course/entities/Order.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.time.Instant;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.kleinbruno.course.entities.enums.OrderStatus;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -25,6 +26,8 @@ public class Order implements Serializable{
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
 	
+	private Integer orderStatus;
+	
 //	@JsonIgnore
 	@ManyToOne
 	@JoinColumn(name = "client_id")
@@ -35,9 +38,10 @@ public class Order implements Serializable{
 	}
 
 
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment,OrderStatus orderStatus, User client) {
 		this.id = id;
 		this.moment = moment;
+		setOrderStatus(orderStatus);;
 		this.client = client;
 	}
 
@@ -59,6 +63,18 @@ public class Order implements Serializable{
 
 	public void setMoment(Instant moment) {
 		this.moment = moment;
+	}
+
+
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if (orderStatus != null) {
+			this.orderStatus = orderStatus.getCode();
+		}
 	}
 
 
