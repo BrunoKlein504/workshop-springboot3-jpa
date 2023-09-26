164cbf0adaa7861c2ffdc9d51b092402a48820b9
BrunoKlein504 <bkleingomes@gmail.com>
2023-09-26 11:25:32 -0300

    Exception handling - update

--- src/main/java/com/kleinbruno/course/services/UserService.java
+++ src/main/java/com/kleinbruno/course/services/UserService.java
@@ -13,6 +13,8 @@ import com.kleinbruno.course.repositories.UserRepository;
 import com.kleinbruno.course.services.exceptions.DatabaseException;
 import com.kleinbruno.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service //@Component
 public class UserService {
 
@@ -44,9 +46,13 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
